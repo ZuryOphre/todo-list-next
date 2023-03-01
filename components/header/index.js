5ac4b7f302c6e661420185d0a9ab6d3eeda14011
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-03-01 00:42:17 -0300

    the application header has been created

---
+++ components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800">
+      <div className="container mx-auto p-4">
+        <h1 className="text-white font-bold text-2xl text-center text-purple-700">To-Do List</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
